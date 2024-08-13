747a3f2079e83b00e444d2bb79e10bd89b0932c3
Neeraj-C08 <neerajchandekar@gmail.com>
2024-08-13 15:07:56 -0700

    Update Program3.java

--- Program3.java
+++ Program3.java
@@ -4,11 +4,60 @@ import java.util.Scanner; //This imports the console scanner that reads user inp
 
 //Your code here:
 
+public class program3{
+
+    public static void main(String []args){
+    
+    int length = 0;
+    
+    int width = 0;
+    
+    int area = 0;
+    
+    int perimeter = 0;
+    
+    Scanner myObj = new Scanner(System.in);
+    
+    System.out.println("Enter the length: ");
+
+      length = myObj.nextInt();
+
+    System.out.println("Enter the width: ");
+    
+     width = myObj.nextInt();
+       
+    System.out.println("The length is: " + length);
+    
+    
+    System.out.println("The width is: " + width);
+    
+    area = length*width;
+    
+    System.out.println("The area is: " + area);
+    
+    perimeter = 2*length + 2*width;
+    
+    System.out.println("The perimeter is: " + perimeter);
+    
+    
+    
+    }
+    
+    
+}
 
 
 
 //Paste console output below:
 /*
+Enter the length: 
+6
+Enter the width: 
+9
+The length is: 6
+The width is: 9
+The area is: 54
+The perimeter is: 30
 
 
 */
